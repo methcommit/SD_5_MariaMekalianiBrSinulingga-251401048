ef8b205144f695e1335d624db71332425045c5ce
anakin.... why? <mariaslinggk@gmail.com>
2026-09-16 22:14:09 +0700

    Add files via upload

---
+++ Pertemuan 1/oleh-oleh.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int arr[3][3][4]; 
+    int nilai = 2;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                arr[i][j][k] = nilai;
+                nilai += 2;
+            }
+        }
+    }
+
+    for (int i = 0; i < 3; i++) {
+        cout << "Lapis ke-" << (i + 1) << ":\n";
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 4; k++) {
+                cout << setw(4) << arr[i][j][k];
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+    }
+}
